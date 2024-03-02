e565917d5c38d32d29ecf5fb667157c2d67f867b
SmolFennec <115507426+SmolFennec@users.noreply.github.com>
2024-03-02 22:14:06 +0000

    FINISHED MOBILE OPTIMISATION

--- script.js
+++ script.js
@@ -11,19 +11,17 @@ window.onload = () => {
 
 function openReviewForm() {
 if (accessToken != null) {
-    document.getElementById("form").style.display = "block";
+    document.getElementById("form").classList.add("show");
 }
 
-document.getElementById("formbox").style.height = "300px";
-document.getElementById("formbox").style.cursor = "default";
+document.getElementById("formbox").classList.add("opensmall")
 document.getElementById("close").style.display = "block";
 }
 
 function closeReviewForm(event) {
 event.stopPropagation();
 document.getElementById("form").style.display = "none";
-document.getElementById("formbox").style.height = "50px";
-document.getElementById("formbox").style.cursor = "pointer";
+document.getElementById("formbox").classList.remove("opensmall")
 document.getElementById("close").style.display = "none";
 }
 
@@ -91,3 +89,28 @@ function CloseAlert() {
     document.getElementById("alert").style.display = "none";
 }
 document.getElementById("alrtcls").addEventListener("click", CloseAlert);
+
+
+let mobileMenu = document.getElementById("mobilemenu");
+document.getElementById("burger").addEventListener("click", function() {
+    if (mobileMenu.style.display === "none" || mobileMenu.style.display === "") {
+        // If menu is closed, open it
+        mobileMenu.style.display = "block";
+        mobileMenu.classList.remove("closed");
+        mobileMenu.classList.add("open");
+        document.body.style.overflow = 'hidden';
+    } else {
+        // If menu is open, close it
+        mobileMenu.classList.remove("open");
+        mobileMenu.classList.add("closed");
+
+        // Wait for the animation to finish before hiding the element
+        setTimeout(function() {
+            if (mobileMenu.classList.contains("closed")) {
+                mobileMenu.style.display = "none";
+            }
+        }, 500); // The timeout should be equal to the duration of the animation
+
+        document.body.style.overflow = 'auto';
+    }
+});
